c4566f424171decf32361e5a6dfad6b08c7edb0f
Ben Pollard <benmacpro@Bens-MacBook-Pro.local>
2021-11-19 16:14:51 +0000

    updates to getting entropy

--- entropy.m
+++ entropy.m
@@ -3,14 +3,38 @@ load Decision-Tree-matlab-cw2/data_banknote_authentication.txt
 X = data_banknote_authentication(:,1:4);
 y = data_banknote_authentication(:,5);
 
+data = data_banknote_authentication;
 
 y = double(y);
 y(y==3) = 1;
 y(y==2) = 0;
 z = X(:,1);
 %max(z)
+frequencyi = get_freq(z);
+frequency = frequencyi(2,:);
+probs = get_prob(frequency);
+c = get_entropy(probs);
 
-c = get_entropy(z);
+%normalize data into categories kind of
+z = round(data(:,1),1);
+
+
+
+
+for i = 1:1:length(z)
+    p = 0;
+    n = 0;
+    for j = 1:1:length(z)
+        if z(i) == z(j)
+            if y(j) == 1
+                p = p + 1;
+            else
+                n = n + 1;
+            end
+        end
+    end
+end
+
 
 
 function [freq] = get_freq(x)
@@ -20,20 +44,18 @@ function [freq] = get_freq(x)
     freq = [binc; counts];
 end
 
-function [probabilities] = get_prob(frequencies)
-    sum_frequencies = sum(frequencies);
+function [probabilities] = get_prob(probability_frequencies)
+    sum_frequencies = sum(probability_frequencies);
     arr = [];
-    for i = 1:1:length(frequencies)
-        probability = frequencies(i)/sum_frequencies;
+    for i = 1:1:length(probability_frequencies)
+        probability = probability_frequencies(i)/sum_frequencies;
         arr(end+1) = probability;
     end
     probabilities = arr;
 end
 
 
-function [entropy] = get_entropy(x)
-    frequency = get_freq(x);
-    frequency = frequency(2,:);
-    probs = get_prob(frequency);
-    entropy = -sum(probs.*log2(probs));
+function [entropy] = get_entropy(p)
+    entropy = -sum(p.*log2(p));
 end
+
