d8f99251054418bae40bd1fd8ec0ec48416983e3
Ben Pollard <benmacpro@Bens-MacBook-Pro.local>
2021-11-19 13:39:13 +0000

    entropy for one attribute is working

--- entropy.m
+++ entropy.m
@@ -8,34 +8,32 @@ y = double(y);
 y(y==3) = 1;
 y(y==2) = 0;
 z = X(:,1);
-max(z)
-c = get_entropy_one_attr(z);
+%max(z)
+
+c = get_entropy(z);
 
 
 function [freq] = get_freq(x)
-    disp(max(x))
-    binc = [min(x):0.1:max(x)];
+    %disp(max(x))
+    binc = [min(x):1:max(x)];
     counts = hist(x,binc);
     freq = [binc; counts];
 end
 
-
-function [entropy] = get_entropy_one_attr(attribute)
+function [probabilities] = get_prob(frequencies)
+    sum_frequencies = sum(frequencies);
     arr = [];
-    freq = get_freq(attribute);
-    arr_prob_i = [];
-    for i = 1:1:length(freq(1,:))
-        prob_i = freq(2,i)/length(attribute);
-        v = prob_i*log2(prob_i);
-        arr(end+1) = -v;
-        arr_prob_i(end+1) = prob_i;
+    for i = 1:1:length(frequencies)
+        probability = frequencies(i)/sum_frequencies;
+        arr(end+1) = probability;
     end
-    sum_prob_i = sum(arr_prob_i);
-    disp(sum_prob_i)
-    arr(find(isnan(arr)))=[];
-    entropy = sum(arr);
+    probabilities = arr;
 end
 
-function [entropy] = get_entropy_all_attr(attribute)
-    
+
+function [entropy] = get_entropy(x)
+    frequency = get_freq(x);
+    frequency = frequency(2,:);
+    probs = get_prob(frequency);
+    entropy = -sum(probs.*log2(probs));
 end
